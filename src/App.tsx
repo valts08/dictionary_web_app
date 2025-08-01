b783644c0448b73818c9a16270f1f456a01daa59
valts08 <valts.pilans@gmail.com>
2025-08-02 00:53:33 +0300

    added tanstack query to the project, for fetching and storing data. tried to set it up, but isn't done yet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import './index.css'
 import TopBar from './components/top-bar/TopBar'
 import { useAppSelector } from './hooks/storeHooks'
-import SearchIcon from './assets/images/icon-search.svg'
 import DictionarySection from './components/main-dict-area/DictionarySection'
-import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
 
   const { fontObj } = useAppSelector(state => state.toggles)
-  const [searchWord, setSearchWord] = useState("")
 
   return (
     <div className={`md:max-w-7/12 md:mx-auto max-w-12/12 mx-5 border-1 border-amber-200 mt-10 ${fontObj.activeFont.font}`}>
       <TopBar/>
-      <div className='flex mt-10 rounded-xl place-content-between bg-black/10'>
-        <input type="text" placeholder='Search for a word...' className='p-3 min-w-8/10 font-bold' onChange={(e) => setSearchWord(e.target.value)} />
-        <img src={SearchIcon} alt="search-icon" className='pr-4 cursor-pointer' />
-      </div>
       <div className='mt-8'>
-        <DictionarySection />
+        <QueryClientProvider client={queryClient}>
+          <DictionarySection />
+        </QueryClientProvider>
       </div>
     </div>
   )
--- src/components/main-dict-area/DictionarySection.tsx
+++ src/components/main-dict-area/DictionarySection.tsx
@@ -1,8 +1,42 @@
+import SearchIcon from '../../assets/images/icon-search.svg'
+import { useState } from 'react'
+import { useQuery } from "@tanstack/react-query"
+
 const DictionarySection = () => {
+
+    const [searchWord, setSearchWord] = useState("")
+
+    const { refetch, isRefetchError, error, data, isRefetching } = useQuery({
+        queryKey: ["dictionary"],
+        queryFn: async () => {
+            const result = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchWord}`)
+            
+            if (!result.hasOwnProperty("word")) {
+                return Promise.reject(new Error(`Couldn't fetch data`))
+            }
+            console.log(result.json())
+            return result.json()
+        },
+        retry: 1,
+        staleTime: Infinity,
+        enabled: false
+    })
 
     return (
         <>
-        
+        <div className='flex mt-10 rounded-xl place-content-between bg-black/10'>
+            <input type="text" placeholder='Search for a word...' className='p-3 min-w-8/10 font-bold' onChange={(e) => setSearchWord(e.target.value)} />
+            <img src={SearchIcon} alt="search-icon" className='pr-4 cursor-pointer' onClick={() => refetch()}/>
+        </div>
+        {data ? (
+            <div>{data[0].word}</div>
+        ) : isRefetchError ? (
+            <div>RefetchError</div>
+        ) : isRefetching ? (
+            <div>...Refetching</div>
+        ) : (
+            <div>Not ready yet/Nothing</div>
+        )}
         </>
     )
 }
