6b900e9bffa15e644fda734b981ba88b49f540ea
valts08 <valts.pilans@gmail.com>
2025-08-04 15:50:51 +0300

    rendering almost all fetched data for search word

--- src/components/main-dict-area/DictionarySection.tsx
+++ src/components/main-dict-area/DictionarySection.tsx
@@ -1,18 +1,20 @@
 import SearchIcon from '../../assets/images/icon-search.svg'
 import { useState } from 'react'
 import { useQuery } from "@tanstack/react-query"
+import AudioPlayBtn from '../../assets/images/icon-play.svg'
+import OpenLinkNewWindow from '../../assets/images/icon-new-window.svg'
 
 interface DictionarySearchResultType {
     license: object,
     meanings: MeaningsObject[],
     phonetic: string,
     phonetics: object[],
-    sourceUrls: object,
+    sourceUrls: string[],
     word: string
 }
 
 type MeaningsObject = {
-    [key: string]: object | string
+    [key: string]: object | string | DefinitionObject[]
 }
 
 type DefinitionObject = {
@@ -49,42 +51,62 @@ const DictionarySection = () => {
             </div>
             {data ? (
                 <div>
-                    {data && data.map((searchResult: DictionarySearchResultType)  => {
+                    {data && data.map((searchResult: DictionarySearchResultType, searchResultId: number)  => {
                         return (
                             <div className=''>
-                                <div className='flex place-content-between pt-8'>
+                                <div className='flex place-content-between pt-8' key={searchResultId}>
                                     <div className='flex flex-col'>
-                                        <h1 className='text-5xl font-bold'>
+                                        <h1 className='text-7xl font-bold'>
                                             {searchResult.word}
                                         </h1>
-                                        <span>
+                                        <span className='pt-5'>
                                             {searchResult.phonetic}
                                         </span>
                                     </div>
-                                    <button className='border-1'>Audio</button>
+                                    <button onClick={() => console.log('pressed play')} className='hover:cursor-pointer'>
+                                        <img src={AudioPlayBtn} alt="pronunciation-play-button" />
+                                    </button>
                                 </div>
-                                {searchResult.meanings.map((meaning: any) => {
+                                {searchResult.meanings.map((meaning: any, meaningId: number) => {
                                     return (
-                                        <div>
-                                            <span>{meaning.partOfSpeech}</span>
-                                            <span className='block'>Meaning:</span>
-                                            <ul>
-                                                {meaning.definitions.map((definition: DefinitionObject) => {
+                                        <div key={meaningId}>
+                                            <span className='block py-6 font-bold'>{meaning.partOfSpeech}</span>
+                                            <span>Meaning:</span>
+                                            <ol className='pt-4 pb-10'>
+                                                {meaning.definitions.map((definitionObject: any, definitionId: number) => {
                                                     return (
-                                                        <div>
-                                                            <li></li>
-                                                        </div>
+                                                        <>
+                                                            <li key={definitionId} className='py-1'>
+                                                                {`${definitionObject.definition}`}
+                                                                <span className='text-red-500 block'>{definitionObject.example}</span>
+                                                            </li>
+                                                        </>
                                                     )
                                                 })}
-                                            </ul>
+                                            </ol>
+                                            {meaning.synonyms.length ? "Synonyms:" : ""}
+                                            {meaning.synonyms.map((synonym: string, synonymId: number) => {
+                                                return (
+                                                    <span className='pl-5 text-purple-700 font-bold' key={synonymId}>{synonym}</span>
+                                                )
+                                            })}
                                         </div>
+                                    )
+                                })}
+                                {searchResult.sourceUrls.length ? "Source:" : ""}
+                                {searchResult.sourceUrls.map((url: string, urlId: number) => {
+                                    return (
+                                        <>
+                                            <a href={url} className='pl-5' key={urlId}>
+                                                {url}
+                                                <img src={OpenLinkNewWindow} alt="open-link-newtab" className='inline max-w-[12px] max-h-[12px]'/>
+                                            </a>
+                                        </>
                                     )
                                 })}
                             </div>
                         )
-                        
                     })}
-                    
                 </div>
             ) : isRefetchError ? (
                 <div>RefetchError</div>
